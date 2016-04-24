7dfd7db26f4fddebbf2fa04790a0e58b667b5dd2
rymotion1@me.com <rymotion1@me.com>
2016-04-23 23:47:54 -0700

    added sample code
    I have added and implemented the sample code from the CSE460 website

--- assembler.cpp
+++ assembler.cpp
@@ -2,16 +2,15 @@
 //Ryan Paglinawan
 #include "assembler.h"
 #include "virtualmachine.h"
-#include <iostream>
-#include <string>
-#include <stdlib.h>
-#include <stdio.h>
-#include <fstream>
 #include <map>
 #include <stdexcept>
 #include <vector>
 #include <fstream>
-//#include "virtualmachine.h"
+#include <cstdlib>  // for exit()
+#include <iostream> // for cout, ...
+#include <fstream>  // for fstream::open()
+#include <string>
+#include <sstream>  // for istringstream
 
 using namespace std;
 
@@ -383,4 +382,35 @@ void Assembler::noop()
 {
 	OP = 25;
 	clock++;
+}
+
+main()
+{
+	fstream assemblyProg;
+	string line, opcode;
+	int rd, rs, constant;
+
+	assemblyProg.open("prog.s", ios::in);
+	if (!assemblyProg.is_open()) {
+		cout << "prog.s failed to open.\n";
+		exit(1);
+	}
+
+	getline(assemblyProg, line);
+	while (!assemblyProg.eof()) {
+		rd=-1; rs=-1; constant=-129; // init to invalid values
+
+		// the following line is the trick
+		istringstream str(line.c_str());
+		str >> opcode;
+
+		if (opcode == "add")
+			str >> rd >> rs;
+		else if (opcode == "addi")
+			str >> rd >> constant;
+		else cout << "Some other opcode: ";
+
+		cout << opcode << " " << rd << " " << rs << " " << constant << endl;
+		getline(assemblyProg, line);
+	}
 }
